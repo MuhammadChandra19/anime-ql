01b3fef643f61488ada157a8d4ee28b658e9fb5e
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2022-08-18 23:35:17 +0700

    init apolo

---
+++ src/query/anime-list.ts
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_MEDIA_LIST = gql`
+  query AnimeList($page: Int, $perPage: Int, $search: String) {
+    Page(page: $page, perPage: $perPage) {
+      pageInfo {
+        total
+        perPage
+      }
+      media {
+        id
+        idMal
+        title {
+          romaji
+          english
+          native
+          userPreferred
+        }
+        bannerImage
+        coverImage {
+          extraLarge
+          large
+          medium
+          color
+        }
+        status # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+      }
+    }
+  }
+`;
