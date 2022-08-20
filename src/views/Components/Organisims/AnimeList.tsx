3e47cb5201df8879147f79397de5e3ee936e1e60
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2022-08-20 23:41:37 +0700

    fix loading state

--- src/views/Components/Organisims/AnimeList.tsx
+++ src/views/Components/Organisims/AnimeList.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import tw, { css } from 'twin.macro'
 import { AnimeList_Page_media } from '../../../graphql/animeList/__generated__/AnimeList'
 import { AnimeContainer, AnimeListItem } from '../../styles/AnimeStyles'
 import AnimeItem from '../Molecules/AnimeItem'
@@ -15,7 +14,13 @@ type AnimeListProps = {
 const AnimeList:React.FC<AnimeListProps> = ({ animeList, isLoading, error, onClick }) => {
   
   if (isLoading) {
-    return <AnimeContainer css={tw`min-h-screen w-full text-center bg-gray-400`}>Loading...</AnimeContainer>
+    return (
+      <AnimeListItem>
+        {
+          new Array(18).fill({}).map((_, idx) => <AnimeItemSkeleton id={idx}/>)
+        }
+      </AnimeListItem>
+    )
         
     
   }
