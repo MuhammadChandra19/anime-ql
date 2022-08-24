121bb3acbc2dd391a5dc96de687ebc9c99e32a5d
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2022-08-24 23:18:21 +0700

    fix naming

--- src/index.tsx
+++ src/index.tsx
@@ -20,8 +20,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<MainLayout />} >
-        <Route index element={<Home />} />
+      <Route element={<MainLayout />} >
+        <Route path="/"  element={<Home />} />
         <Route path="detail/:id" element={<Detail />} />
         <Route path="collection" element={<Collection />}/>
         <Route path='collection/:id' element={<CollectionDetail />}/>
